708f8465ad03906a981378bc34dc71455d647784
Per Kraulis <per.kraulis@gmail.com>
2025-04-14 14:54:12 +0200

    Minor fix.

--- neochart/cli.py
+++ neochart/cli.py
@@ -16,23 +16,26 @@ def cli():
 
 
 @cli.command()
+@click.option("-i", "--indent", default=2)
 @click.argument("infilepath", nargs=1, required=True)
 @click.argument("outfilepath", nargs=1, required=False)
-def svg(infilepath, outfilepath=None):
+def svg(indent, infilepath, outfilepath=None):
     if not outfilepath:
         outfilepath = Path(infilepath).with_suffix(".svg")
-    core.read(infilepath).write(outfilepath)
+    with open(outfilepath, "w") as outfile:
+        core.read(infilepath).svg().write(outfile, indent=indent)
 
 
 @cli.command()
+@click.option("-s", "--scale", default=1.0, type=float)
 @click.argument("infilepath", nargs=1, required=True)
 @click.argument("outfilepath", nargs=1, required=False)
-def png(infilepath, outfilepath=None):
+def png(scale, infilepath, outfilepath=None):
     if not outfilepath:
         outfilepath = Path(infilepath).with_suffix(".png")
     buffer = io.StringIO(repr(core.read(infilepath).svg()))
     with open(outfilepath, "wb") as outfile:
-        outfile.write(cairosvg.svg2png(file_obj=buffer))
+        outfile.write(cairosvg.svg2png(file_obj=buffer, scale=scale))
 
 
 if __name__ == "__main__":
